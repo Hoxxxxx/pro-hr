4d3f8178951276e844fcef54e612132322c3a656
侯鑫 <1239840541hox@gmail.com>
2020-09-14 11:39:44 +0800

    员工转正页面开发

--- src/utils/request.js
+++ src/utils/request.js
@@ -39,45 +39,45 @@ axios.interceptors.response.use(
     }, err => {
         if (err && err.response) {
             switch (err.response.status) {
-                case 400:
-                    console.log('错误请求')
-                    break;
-                case 401:
-                    sessionStorage.clear();
-                    window.location = '/login'
-                    break;
-                case 403:
-                    window.location = '/403'
-                    console.log('无权限访问')
-                    break;
-                case 404:
-                    window.location = '/404'
-                    console.log('请求错误,未找到该资源')
-                    break;
-                case 405:
-                    console.log('请求方法未允许')
-                    break;
-                case 408:
-                    console.log('请求超时')
-                    break;
-                case 500:
-                    console.log(`服务器端出错：${err.response.data.message}`)
-                    break;
-                case 501:
-                    console.log('网络未实现')
-                    break;
-                case 502:
-                    console.log('网络错误')
-                    break;
-                case 503:
-                    console.log('服务不可用')
-                    break;
-                case 504:
-                    console.log('网络超时')
-                    break;
-                case 505:
-                    console.log('http版本不支持该请求')
-                    break;
+                // case 400:
+                //     console.log('错误请求')
+                //     break;
+                // case 401:
+                //     sessionStorage.clear();
+                //     window.location = '/login'
+                //     break;
+                // case 403:
+                //     window.location = '/403'
+                //     console.log('无权限访问')
+                //     break;
+                // case 404:
+                //     window.location = '/404'
+                //     console.log('请求错误,未找到该资源')
+                //     break;
+                // case 405:
+                //     console.log('请求方法未允许')
+                //     break;
+                // case 408:
+                //     console.log('请求超时')
+                //     break;
+                // case 500:
+                //     console.log(`服务器端出错：${err.response.data.message}`)
+                //     break;
+                // case 501:
+                //     console.log('网络未实现')
+                //     break;
+                // case 502:
+                //     console.log('网络错误')
+                //     break;
+                // case 503:
+                //     console.log('服务不可用')
+                //     break;
+                // case 504:
+                //     console.log('网络超时')
+                //     break;
+                // case 505:
+                //     console.log('http版本不支持该请求')
+                //     break;
                 default:
                     console.log(`连接错误${err.response.status}`)
             }
--- src/views/staffManage/staffMsg.vue
+++ src/views/staffManage/staffMsg.vue
@@ -44,13 +44,13 @@
               <div class="labelBox">
                 <span class="label">手机号</span>
               </div>
-               <div class="elInput">具体信息</div>
+              <div class="elInput">具体信息</div>
             </div>
             <div class="itemBox">
               <div class="labelBox">
                 <span class="label">身份证号</span>
               </div>
-               <div class="elInput">具体信息</div>
+              <div class="elInput">具体信息</div>
             </div>
           </li>
           <!-- 出生日期/邮箱 -->
@@ -60,13 +60,13 @@
                 <span class="redPot"></span>
                 <span class="label">出生日期</span>
               </div>
-               <div class="elInput">具体信息</div>
+              <div class="elInput">具体信息</div>
             </div>
             <div class="itemBox">
               <div class="labelBox">
                 <span class="label">邮箱</span>
               </div>
-               <div class="elInput">具体信息</div>
+              <div class="elInput">具体信息</div>
             </div>
           </li>
         </ul>
@@ -81,13 +81,13 @@
               <div class="labelBox">
                 <span class="label">员工状态</span>
               </div>
-               <div class="elInput">具体信息</div>
+              <div class="elInput">具体信息</div>
             </div>
             <div class="itemBox">
               <div class="labelBox">
                 <span class="label">所属公司</span>
               </div>
-               <div class="elInput">具体信息</div>
+              <div class="elInput">具体信息</div>
             </div>
           </li>
           <!-- 部门/职位 -->
@@ -96,13 +96,13 @@
               <div class="labelBox">
                 <span class="label">部门</span>
               </div>
-               <div class="elInput">具体信息</div>
+              <div class="elInput">具体信息</div>
             </div>
             <div class="itemBox">
               <div class="labelBox">
                 <span class="label">职位</span>
               </div>
-               <div class="elInput">具体信息</div>
+              <div class="elInput">具体信息</div>
             </div>
           </li>
           <!-- 入职时间/转正时间 -->
@@ -111,13 +111,13 @@
               <div class="labelBox">
                 <span class="label">入职时间</span>
               </div>
-               <div class="elInput">具体信息</div>
+              <div class="elInput">具体信息</div>
             </div>
             <div class="itemBox">
               <div class="labelBox">
                 <span class="label">转正时间</span>
               </div>
-               <div class="elInput">具体信息</div>
+              <div class="elInput">具体信息</div>
             </div>
           </li>
           <!-- 试用期/工号 -->
@@ -126,13 +126,13 @@
               <div class="labelBox">
                 <span class="label">试用期</span>
               </div>
-               <div class="elInput">具体信息</div>
+              <div class="elInput">具体信息</div>
             </div>
             <div class="itemBox">
               <div class="labelBox">
                 <span class="label">工号</span>
               </div>
-               <div class="elInput">具体信息</div>
+              <div class="elInput">具体信息</div>
             </div>
           </li>
         </ul>
@@ -147,13 +147,13 @@
               <div class="labelBox">
                 <span class="label">婚姻状况</span>
               </div>
-               <div class="elInput">具体信息</div>
+              <div class="elInput">具体信息</div>
             </div>
             <div class="itemBox">
               <div class="labelBox">
                 <span class="label">学历</span>
               </div>
-               <div class="elInput">具体信息</div>
+              <div class="elInput">具体信息</div>
             </div>
           </li>
           <!-- 紧急联系人 -->
@@ -162,7 +162,7 @@
               <div class="labelBox">
                 <span class="label">紧急联系人</span>
               </div>
-               <div class="elInput">具体信息</div>
+              <div class="elInput">具体信息</div>
             </div>
           </li>
         </ul>
@@ -170,6 +170,123 @@
       <div class="btnBox">
         <div class="btns">
           <el-button style="width:95px;" type="primary" @click="editMsg()">编辑</el-button>
+        </div>
+      </div>
+    </el-card>
+    <!-- 转正信息 -->
+    <el-card class="formCard" v-if="curIndex == 1">
+      <!-- 转正信息填写 -->
+      <div class="positiveEdit" v-if="positiveStatus != 0">
+        <div class="baseInfo">
+          <ul class="inputBox">
+            <!-- 姓名/入职日期 -->
+            <li v-if="positiveStatus == 1">
+              <div class="itemBox">
+                <div class="labelBox">
+                  <span class="label">姓名</span>
+                </div>
+                <div class="elInput">具体信息</div>
+              </div>
+              <div class="itemBox">
+                <div class="labelBox">
+                  <span class="label">入职日期</span>
+                </div>
+                <div class="elInput">具体信息</div>
+              </div>
+            </li>
+            <!-- 手机号 -->
+            <li v-if="positiveStatus == 1">
+              <div class="itemBox">
+                <div class="labelBox">
+                  <span class="label">手机号</span>
+                </div>
+                <div class="elInput">具体信息</div>
+              </div>
+            </li>
+            <!-- 申请时间/试用期 -->
+            <li v-if="positiveStatus == 2">
+              <div class="itemBox">
+                <div class="labelBox">
+                  <span class="label">申请时间</span>
+                </div>
+                <div class="elInput">具体信息</div>
+              </div>
+              <div class="itemBox">
+                <div class="labelBox">
+                  <span class="label">试用期</span>
+                </div>
+                <div class="elInput">具体信息</div>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <!-- 转正时间 -->
+        <div class="baseInfo">
+          <ul class="inputBoxPositive">
+            <!-- 转正时间 -->
+            <li v-if="positiveStatus == 1">
+              <div class="itemBox">
+                <div class="labelBox">
+                  <span class="redPot">&#10052;</span>
+                  <span class="label">转正时间</span>
+                </div>
+                <el-date-picker
+                  v-model="positiveTime"
+                  type="date"
+                  placeholder="选择转正时间"
+                  class="elInput"
+                ></el-date-picker>
+              </div>
+            </li>
+          </ul>
+          <!-- 工作总结 -->
+          <div class="conclusion">
+            <span class="label">工作总结</span>
+            <el-input
+              type="textarea"
+              :rows="25"
+              placeholder="请输入内容"
+              minlength="30"
+              v-model="conclusion"
+              :disabled="positiveStatus == 1 ? false : true"
+            ></el-input>
+          </div>
+          <div class="upload">
+            <span class="label">附件</span>
+            <el-upload
+              class="upload-demo"
+              action="https://jsonplaceholder.typicode.com/posts/"
+              :on-preview="handlePreview"
+              :on-remove="handleRemove"
+              :before-remove="beforeRemove"
+              multiple
+              :limit="3"
+              :on-exceed="handleExceed"
+              :file-list="fileList"
+              v-if="positiveStatus == 1"
+            >
+              <el-button size="small" type="primary" style="width:120px;">新增附件</el-button>
+            </el-upload>
+            <ul class="fileList" v-if="positiveStatus == 2">
+              <li v-for="(item,index) in fileList" :key="index">
+                <span class="fileName">文件名</span>
+                <span class="fileDownload">下载</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="btnBox" v-if="positiveStatus == 1">
+          <div class="btns">
+            <el-button style="width:95px;">取消</el-button>
+            <el-button style="width:95px;" type="primary" @click="positiveSave()">保存</el-button>
+          </div>
+        </div>
+      </div>
+      <!-- 未转正 -->
+      <div class="positiveNull" v-if="positiveStatus == 0">
+        <div>
+          <img src="../../assets/img/dataNull.png" />
+          <el-button style="width:95px;" type="primary" @click="positive()">转正</el-button>
         </div>
       </div>
     </el-card>
@@ -200,7 +317,12 @@ export default {
         { name: "离职信息", id: 2 },
       ],
       curIndex: 0,
-      gender:1,
+      gender: 1,
+      // 转正信息填写部分数据
+      positiveStatus: 0,
+      positiveTime: "", //转正时间
+      conclusion: "", //工作总结
+      fileList: [], //附件
     };
   },
   mounted() {},
@@ -209,10 +331,35 @@ export default {
     changeStatus(index, status) {
       this.curIndex = index;
     },
-    editMsg(){
-        this.$router.push({
-            path:'/staffAdd'
-        })
+    editMsg() {
+      this.$router.push({
+        path: "/staffAdd",
+      });
+    },
+    // 文件上传
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handlePreview(file) {
+      console.log(file);
+    },
+    handleExceed(files, fileList) {
+      this.$message.warning(
+        `当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${
+          files.length + fileList.length
+        } 个文件`
+      );
+    },
+    beforeRemove(file, fileList) {
+      return this.$confirm(`确定移除 ${file.name}？`);
+    },
+    // 转正按钮点击
+    positive(){
+      this.positiveStatus = 1
+    },
+    // 保存转正信息
+    positiveSave(){
+      this.positiveStatus = 2
     }
   },
   components: {
@@ -290,6 +437,7 @@ export default {
       font-size: 16px;
       color: #999999;
     }
+
     .baseInfo {
       border-bottom: 1px solid #f0f3f7;
       margin-bottom: 20px;
@@ -299,7 +447,6 @@ export default {
         flex-wrap: wrap;
         margin-top: 20px;
         li {
-          width: 100%;
           margin-right: 50px;
           display: flex;
           flex-direction: row;
@@ -346,6 +493,95 @@ export default {
           }
         }
       }
+      .inputBoxPositive {
+        li {
+          width: 100%;
+          margin-right: 50px;
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          margin-bottom: 20px;
+          .itemBox {
+            width: 480px;
+            max-width: 500px;
+            min-width: 450px;
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            align-items: center;
+            margin-right: 120px;
+            .elInput {
+              width: 370px;
+            }
+            .labelBox {
+              width: 120px;
+              .redPot,
+              .label {
+                letter-spacing: 1px;
+                color: #f56c6c;
+                font-size: 16px;
+                font-weight: bold;
+                margin-right: 2px;
+              }
+              .label {
+                color: #333333;
+                margin-right: 0;
+              }
+            }
+          }
+        }
+      }
+      .conclusion,
+      .upload {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+
+        .label {
+          display: block;
+          width: 120px;
+          margin-right: 2px;
+          letter-spacing: 1px;
+          font-size: 16px;
+          font-weight: bold;
+          margin-right: 8px;
+        }
+      }
+      .upload {
+        align-items: normal;
+        justify-content: flex-start;
+        .label {
+          margin-right: 0;
+          line-height: 32px;
+        }
+        .fileList {
+          li {
+            margin-bottom: 10px;
+            span {
+              display: inline-block;
+              height: 32px;
+              line-height: 32px;
+            }
+            .fileName {
+              font-size: 14px;
+              color: #3b4859;
+            }
+            .fileDownload {
+              width: 60px;
+              height: 20px;
+              cursor: pointer;
+              line-height: 20px;
+              color: #fff;
+              background: #409efd;
+              text-align: center;
+              border-radius: 4px;
+              margin-left: 20px;
+            }
+          }
+        }
+      }
     }
     .btnBox {
       .btns {
@@ -356,6 +592,23 @@ export default {
         display: flex;
         flex-direction: row;
         justify-content: space-evenly;
+      }
+    }
+
+    .positiveNull {
+      min-height: 650px;
+      position: relative;
+      div {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+      }
+      img{
+        margin-bottom: 20px;
       }
     }
   }
