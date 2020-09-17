98ca18cece6f45fdb02dda19c9ad9617871fb445
侯鑫 <1239840541hox@gmail.com>
2020-09-17 17:00:42 +0800

    新增接口

--- src/api/configUrl.js
+++ src/api/configUrl.js
@@ -9,6 +9,7 @@ let configUrl = {
     staffCount:'/api/users/statusCount',//列表页上方切换区域人数统计
     getDepartments: '/api/departmentsAll',//新增员工页面获取所有部门列表
     getJobs:'/api/positions',//新增员工页面获取所有职位列表
+    positiveSave:'/api/positives',//转正保存
 }
 
 export default configUrl
