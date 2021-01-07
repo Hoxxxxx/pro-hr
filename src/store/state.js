e5512d532ff541c735537d4bc07817caf608553e
侯鑫 <houxin@qq.com>
2021-01-07 15:27:30 +0800

    修改正式环境域名

--- src/store/state.js
+++ src/store/state.js
@@ -3,8 +3,8 @@ const state = {
   userInfo: userInfo,
   permissionList: [],
   // 上传地址
-  upload_url: 'http://test-erp.hualumedia.com/api/v2/hr/files/upload', // 卢星宇：仅文件上传——应收账款上传
-  upload_pic_url: 'http://test-erp.hualumedia.com/api/v2/files/upload',// 张继东：允许多类型上传
-  upload_scan: 'http://test-erp.hualumedia.com/api/v2/finance/bank-slips/upload',// 陈仁政：扫描件上传（可返回扫描结果）
+  upload_url: `${process.env.VUE_APP_BASE_API}hr/files/upload`, // 卢星宇：仅文件上传——应收账款上传
+  upload_pic_url: `${process.env.VUE_APP_BASE_API}files/upload`,// 张继东：允许多类型上传
+  upload_scan: `${process.env.VUE_APP_BASE_API}finance/bank-slips/upload`,// 陈仁政：扫描件上传（可返回扫描结果）
 }
 export default state
